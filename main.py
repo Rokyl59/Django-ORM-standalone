13ffcb525f6f19a0835af2fbb3f092aaa18cbf18
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-03-29 14:36:23 +0500

    Initial

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard, Visit  # noqa: E402
+
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Всего пропусков:', Passcard.objects.count())  # noqa: T001
+
+    active_user = Passcard.objects.filter(is_active=True)
+    print(f'Активных пропусков: {len(active_user)}')
+
+    user_not_leaved = Visit.objects.filter(leaved_at__isnull=True)
+    print(user_not_leaved)
